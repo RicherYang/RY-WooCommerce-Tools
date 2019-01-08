6a89fc1fd1711f05f46de82901b3858e3122ba49
RicherYang <fantasyworldidvtw@gmail.com>
2019-01-08 17:24:33 +0800

    update 1.0.7.1

--- style/js/admin/ry_ecpay_shipping.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($) {
-	if( $('#ry-ecpoay-shipping-info').length ) {
-		$('a.load_customer_shipping').remove();
-		$('a.billing-same-as-shipping').remove();
-	}
-});
--- templates/cart/cart-chouse-cvs.php
+++
@@ -1,6 +0,0 @@
-<tr class="chouse_cvs">
-	<th><?php echo __('Convenience store', 'ry-woocommerce-tools') ?></th>
-	<td data-title="<?php echo esc_attr__('Chouse Cvs Store', 'ry-woocommerce-tools') ?>">
-		<p><button type="button" class="button" onclick="RYECPaySendCvsPost();"><?php echo __('Chouse convenience store', 'ry-woocommerce-tools') ?></button></p>
-	</td>
-</tr>
