31633289ca48dc897a75bb84e025ab7aecac568e
Richer Yang <fantasyworldidvtw@gmail.com>
2026-09-23 17:33:38 +0800

    Update gateway-paid-order-failed.php

--- templates/emails/gateway-paid-order-failed.php
+++ templates/emails/gateway-paid-order-failed.php
@@ -10,7 +10,7 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @version 2026.9.21
+ * @version 2026.9.22
  */
 
 /**
